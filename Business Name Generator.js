718ac22e8b16f4ca3a0c5365ec6da76314e562cc
ricky wasnik <clipaclip321@gmail.com>
2025-05-25 16:59:53 +0530

    Business Name Generator
    Create a  business name generator by combining list of adjectives
    // and shop name and another word
    
    // Adjectives:
      // Crazy
      // Amazing
      // Fire
    
    // Shop Name:
      // Engine
      // Foods
      // Garments
    
    // Another Word:
      // Bros
      // Limited
      // Hub
    
      //no array methods allowed

---
+++ Business Name Generator.js
@@ -0,0 +1,149 @@
+// Create a  busi ness name generator by combining list of adjectives
+// and shop name and another word
+
+// Adjectives:
+  // Crazy
+  // Amazing
+  // Fire
+
+// Shop Name:
+  // Engine
+  // Foods
+  // Garments
+
+// Another Word:
+  // Bros
+  // Limited
+  // Hub
+
+  //no array methods allowed
+console.log("\n Business Name Generator!!\n");
+  let a  = "crazy ";
+  let b  = "Amazing";
+  let c  ="fire";
+
+  let i="Engine";
+  let j ="Foods";
+  let k="Garments";
+
+  let x ="Bros";
+  let y="Limited";
+  let z="Hub";
+
+  if (Math.random() < 0.037)
+ 
+  {
+ console.log(`${a}  ${i } ${x}`);
+  } 
+  else if (Math.random()< 0.074){
+
+    console.log(`${a} ${i}  ${y}`)
+  }
+  else if (Math.random()< 0.111 )
+    {
+     console.log(`${a} ${i} ${z}`);
+    } 
+    
+    
+    else if (Math.random() < 0.148)
+         {
+            console.log(`${a} ${j} ${x}`);
+    }
+    else if (Math.random() < 0.185)
+         {
+                console.log(`${a} ${j} ${y}`);
+        }
+        else if (Math.random() < 0.222)
+             {
+            console.log(`${a} ${j} ${z}`);
+        }
+
+        else if (Math.random() < 0.259)
+             {
+            console.log(`${a} ${k} ${x}`);
+
+        }
+        else if (Math.random() < 0.296)
+             {
+            console.log(`${a} ${k} ${y}`);
+        }
+        else if (Math.random() < 0.333)
+             {
+            console.log(`${a} ${k} ${z}`);
+        }
+        else if (Math.random() < 0.370)
+             {
+            console.log(`${b} ${i} ${x}`);
+        }
+        else if (Math.random() < 0.407)
+             {
+            console.log(`${b} ${i} ${y}`);
+        }
+        else if (Math.random() < 0.444)
+             {
+            console.log(`${b} ${i} ${z}`);
+        }
+        else if (Math.random() < 0.481)
+             {
+            console.log(`${b} ${j} ${x}`);
+        }
+        else if (Math.random() < 0.518)
+             {
+            console.log(`${b} ${j} ${y}`);
+        }
+        else if (Math.random() < 0.555)
+             {
+            console.log(`${b} ${j} ${z}`);
+        }
+        else if (Math.random() < 0.592)
+             {
+            console.log(`${b} ${k} ${x}`);
+        }
+        else if (Math.random() < 0.629)
+             {
+            console.log(`${b} ${k} ${y}`);
+        }
+        else if (Math.random() < 0.666)
+             {
+            console.log(`${b} ${k} ${z}`);
+        }
+        else if (Math.random() < 0.703)
+             {
+            console.log(`${c} ${i} ${x}`);
+        }
+        else if (Math.random() < 0.740)
+             {
+            console.log(`${c} ${i} ${y}`);
+        }
+        else if (Math.random() < 0.777)
+             {
+            console.log(`${c} ${i} ${z}`);
+        }
+        else if (Math.random() < 0.814)
+             {
+            console.log(`${c} ${j} ${x}`);
+        }
+        else if (Math.random() < 0.851)
+             {
+            console.log(`${c} ${j} ${y}`);
+        }
+        else if (Math.random() < 0.888)
+             {
+            console.log(`${c} ${j} ${z}`);
+        }
+        else if (Math.random() < 0.925)
+             {
+            console.log(`${c} ${k} ${x}`);
+        }
+        else if (Math.random() < 0.962)
+             {
+            console.log(`${c} ${k} ${y}`);
+        }
+        else if (Math.random()< 0.999){
+          console.log(`${c} ${k} ${z}`);
+        }
+
+       console.log("\n Do you want to generate another business name? \n");
+
+
+ 
---
+++ faulty_calculator.js
@@ -0,0 +1,126 @@
+// if ( Math.random() < 0.1){
+
+
+//     console.log("hehe");
+//     function add(a,b)
+//     {
+//         console.log(a-b);
+//         return (a-b);
+//     }
+
+//     function mul(a,b)
+//     {
+//         console.log(a+b);
+//         return (a+b);
+//     }
+
+//     function sub(a,b)
+//     {
+//         console.log(a/b);
+//         return (a/b);
+//     }
+
+//     function div(a,b)
+//     {
+//         console.log(a**b);
+//         return (a**b);
+//     }
+
+//     function exp(a,b)
+//     {
+//         console.log(a**b);
+//         return (a**b);
+//     }
+
+// }
+
+
+// else{
+//     function add(a,b)
+//     {
+//         console.log(a+b);
+//         return (a+b);
+//     }
+    
+//     function mul(a,b)
+//     {
+//         console.log(a*b);
+//         return (a*b);
+//     }
+    
+//     function sub(a,b)
+//     {
+//         console.log(a-b);
+//         return (a-b);
+//     }
+    
+//     function div(a,b)
+//     {
+//         console.log(a/b);
+//         return (a/b);
+//     }
+//     function exp(a,b)
+//     {
+//         console.log(a**b);
+//         return (a**b);
+//     }
+
+// }
+// add(4,5);
+
+if (Math.random() < 0.1) {
+    console.log("hehe");
+
+    function add(a, b) {
+        console.log(a - b);
+        return a - b;
+    }
+
+    function mul(a, b) {
+        console.log(a + b);
+        return a + b;
+    }
+
+    function sub(a, b) {
+        console.log(a / b);
+        return a / b;
+    }
+
+    function div(a, b) {
+        console.log(a ** b);
+        return a ** b;
+    }
+
+    function exp(a, b) {
+        console.log(a ** b);
+        return a ** b;
+    }
+
+} else {
+    function add(a, b) {
+        console.log(a + b);
+        return a + b;
+    }
+
+    function mul(a, b) {
+        console.log(a * b);
+        return a * b;
+    }
+
+    function sub(a, b) {
+        console.log(a - b);
+        return a - b;
+    }
+
+    function div(a, b) {
+        console.log(a / b);
+        return a / b;
+    }
+
+    function exp(a, b) {
+        console.log(a ** b);
+        return a ** b;
+    }
+}
+
+add(4, 5);
