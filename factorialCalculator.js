6f49dd8e42935a12a707a163e5afda7f97d18dad
Diyansh wasnik <clipaclip321@gmail.com>
2025-06-01 08:43:27 +0530

    factorial calculator in HTML and JavaScript

---
+++ factorialCalculator.js
@@ -0,0 +1,16 @@
+
+let no= 3; // Change this value to calculate factorial of a different number
+
+let arr=[];
+
+for(let i=0; i<no;i++){
+arr.push(i+1);
+}
+
+
+const red = (a,b)=> {
+  return  a * b;
+}
+
+// console.log(arr);
+console.log(arr.reduce(red));
